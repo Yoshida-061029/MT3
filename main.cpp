e3f0dd58c24db34138b6a63d212148da2666611b
GC2C_08_ヨシダ_ハルキ <k025g1002@g.neec.ac.jp>
2026-05-12 10:37:40 +0900

    MT3_00_04

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <Novice.h>
+#include <cmath>
 
 const char kWindowTitle[] = "GC2C_08_ヨシダ_ハルキ";
 
-struct Vector2 {
+struct Vector3 {
 	float x;
 	float y;
-};
-
-struct Vector3 {
-	float x, y, z;
+	float z;
 };
 
 struct Matrix4x4 {
@@ -16,57 +14,62 @@ struct Matrix4x4 {
 };
 
 
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
-	Matrix4x4 mat = {};
-	mat.m[0][0] = 1.0f;
-	mat.m[1][1] = 1.0f;
-	mat.m[2][2] = 1.0f;
-	mat.m[3][3] = 1.0f;
-	mat.m[3][0] = translate.x;
-	mat.m[3][1] = translate.y;
-	mat.m[3][2] = translate.z;
-	return mat;
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 result = {};
+	result.m[0][0] = 1.0f;
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	result.m[3][3] = 1.0f;
+	return result;
 }
 
 
-Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
-	Matrix4x4 mat = {};
-	mat.m[0][0] = scale.x;
-	mat.m[1][1] = scale.y;
-	mat.m[2][2] = scale.z;
-	mat.m[3][3] = 1.0f;
-	return mat;
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 result = {};
+	result.m[0][0] = std::cos(radian);
+	result.m[0][2] = -std::sin(radian);
+	result.m[1][1] = 1.0f;
+	result.m[2][0] = std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	result.m[3][3] = 1.0f;
+	return result;
 }
 
 
-Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
-	Vector3 result = {};
-	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
-	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
-	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
-	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
-	result.x /= w;
-	result.y /= w;
-	result.z /= w;
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 result = {};
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	result.m[2][2] = 1.0f;
+	result.m[3][3] = 1.0f;
+	return result;
+}
+
+
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result = {};
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
 	return result;
 }
 
 
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
-
-void VectorScreenPrintf(int x, int y, const Vector3& v, const char* label) {
-	Novice::ScreenPrintf(x, y, "%.2f", v.x);
-	Novice::ScreenPrintf(x + kColumnWidth, y, "%.2f", v.y);
-	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.2f", v.z);
-	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
-}
-
-void MatrixScreenPrintf(int x, int y, const Matrix4x4& mat, const char* label) {
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
 	Novice::ScreenPrintf(x, y, "%s", label);
 	for (int row = 0; row < 4; row++) {
-		for (int col = 0; col < 4; col++) {
-			Novice::ScreenPrintf(x + col * kColumnWidth, y + (row + 1) * kRowHeight, "%.2f", mat.m[row][col]);
+		for (int column = 0; column < 4; column++) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%6.02f", matrix.m[row][column]);
 		}
 	}
 }
@@ -78,21 +81,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-
-	Vector3 translate{4.1f, 2.6f, 0.8f};
-	Vector3 scale{1.5f, 5.2f, 7.3f};
-
-	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
-
-	Vector3 point{2.3f, 3.8f, 1.4f};
-
-	Matrix4x4 transformMatrix = {
-	    {{1.0f, 2.0f, 3.0f, 4.0f}, {3.0f, 1.0f, 1.0f, 2.0f}, {1.0f, 4.0f, 2.0f, 3.0f}, {2.0f, 2.0f, 1.0f, 3.0f}}
-    };
-
-	Vector3 transformed = Transform(point, transformMatrix);
-
 	while (Novice::ProcessMessage() == 0) {
 		Novice::BeginFrame();
 
@@ -103,6 +91,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 rotate{0.4f, 1.43f, -0.8f};
+
+		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -111,9 +106,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, transformed, "transformed");
-		MatrixScreenPrintf(0, kRowHeight, translateMatrix, "translateMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 7, scaleMatrix, "scaleMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 0, rotateXMatrix, "rotateXMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix, "rotateYMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix, "rotateZMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix, "rotateXYZMatrix");
 
 		///
 		/// ↑描画処理ここまで
